993d3e3f9767c9d7228261a7014b8a4045b5abe6
Steve <lameres190@potsdam.edu>
2010-11-14 11:17:14 -0500

    Added support for multistage capistrano BUT commented out since it fails

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,1 +1,12 @@
-server 'blackbox', :app, :web, :db, :primary => true
+# 
+# set :stages, %w(blackbox marcy)
+# set :default_stage, 'blackbox'
+# 
+# require 'capistrano/ext/multistage'
+
+# NOTE: the above for multistage capistrano WON'T WORK, it seems to be a bug
+# for rails 3 and moonshine/capistrano-ext
+
+# to deploy to a separate server (e.g blackbox, switch the line below)
+
+server 'marcy', :app, :web, :db, :primary => true
---
+++ config/deploy/blackbox.rb
@@ -0,0 +1,1 @@
+server 'blackbox', :app, :web, :db, :primary => true
